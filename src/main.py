1c810700f7c68104c43977ffe088121849a06202
Unappendixed <38593737+unappendixed@users.noreply.github.com>
2024-05-22 22:43:36 -0700

    start chapter 4

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
-from textnode import TextNode
+from textnode import TextNode, split_nodes_image
 
 
 def main():
-    node = TextNode("Testing", "bold", "https://google.com")
-    print(node)
+    node = TextNode(
+        "I ain't the ![sharpest tool](assets/spoon.png) in the ![shed](assets/outhouse.png)",
+        "text",
+    )
+    expected = [
+        TextNode("I ain't the ", "text"),
+        TextNode("sharpest tool", "image", "assets/spoon.png"),
+        TextNode(" in the ", "text"),
+        TextNode("shed", "image", "assets/outhouse.png"),
+    ]
+    print(split_nodes_image([node]))
+    print("=====")
+    print(expected)
 
 
 if __name__ == "__main__":
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,9 +3,13 @@ from leafnode import LeafNode
 from textnode import (
     TextNode,
     extract_markdown_links,
+    extract_markdown_images,
     split_nodes_delimiter,
+    split_nodes_link,
+    split_nodes_image,
     text_node_to_html_node,
-    extract_markdown_images
+    text_to_textnodes,
+    markdown_to_blocks,
 )
 
 
@@ -91,20 +95,117 @@ class TestTextNode(unittest.TestCase):
             TextNode("gusto", "italic"),
             TextNode(" for a change.", "text"),
         ]
-
         self.assertEqual(split_nodes_delimiter([node], "*", "italic"), expected)
 
     def test_extract_markdown_images_single(self):
         text = "This is some ![markdown text](images/snippet.png) with an image."
-        expected = [TextNode("markdown text", "image", "images/snippet.png")]
+        expected = [("markdown text", "images/snippet.png")]
         self.assertEqual(extract_markdown_images(text), expected)
         pass
 
     def test_extract_markdown_links_single(self):
         text = "Check out this [website](https://links.biz/nice-one) for more."
-        expected = [TextNode("website", "link", "https://links.biz/nice-one")]
+        expected = [("website", "https://links.biz/nice-one")]
         self.assertEqual(extract_markdown_links(text), expected)
         pass
+
+    def test_split_nodes_image_single(self):
+        node = TextNode(
+            "Somebody ![once](assets/once-upon-a-time.png) told me.", "text"
+        )
+        expected = [
+            TextNode("Somebody ", "text"),
+            TextNode("once", "image", "assets/once-upon-a-time.png"),
+            TextNode(" told me.", "text"),
+        ]
+        self.assertEqual(split_nodes_image([node]), expected)
+
+    def test_split_nodes_image_ending(self):
+        node = TextNode(
+            "the world was gonna ![roll me](assets/rolling-barrel.png)", "text"
+        )
+        expected = [
+            TextNode("the world was gonna ", "text"),
+            TextNode("roll me", "image", "assets/rolling-barrel.png"),
+        ]
+        self.assertEqual(split_nodes_image([node]), expected)
+
+    def test_split_nodes_image_double(self):
+        node = TextNode(
+            "![I](assets/eyeball.jpg) ain't the ![sharpest tool](assets/spoon.png) in the ![shed](assets/outhouse.png)",
+            "text",
+        )
+        expected = [
+            TextNode("I", "image", "assets/eyeball.jpg"),
+            TextNode(" ain't the ", "text"),
+            TextNode("sharpest tool", "image", "assets/spoon.png"),
+            TextNode(" in the ", "text"),
+            TextNode("shed", "image", "assets/outhouse.png"),
+        ]
+        self.assertEqual(split_nodes_image([node]), expected)
+
+    def test_split_nodes_link_single(self):
+        node = TextNode(
+            "there is a [house](https://wikipedia.org/House) in New Orleans", "text"
+        )
+        expected = [
+            TextNode("there is a ", "text"),
+            TextNode("house", "link", "https://wikipedia.org/House"),
+            TextNode(" in New Orleans", "text"),
+        ]
+        self.assertEqual(split_nodes_link([node]), expected)
+
+    def test_split_nodes_link_ending(self):
+        node = TextNode(
+            "rats, rats, we are [the rats](https://youtube.com/watch?v=123asdf)", "text"
+        )
+        expected = [
+            TextNode("rats, rats, we are ", "text"),
+            TextNode("the rats", "link", "https://youtube.com/watch?v=123asdf"),
+        ]
+        self.assertEqual(split_nodes_link([node]), expected)
+
+    def test_split_nodes_link_double(self):
+        node = TextNode(
+            "[celebrating](https://wikipedia.org/Celebration) yet another [birthday bash](https://wikipedia.org/Party)",
+            "text",
+        )
+        expected = [
+            TextNode("celebrating", "link", "https://wikipedia.org/Celebration"),
+            TextNode(" yet another ", "text"),
+            TextNode("birthday bash", "link", "https://wikipedia.org/Party"),
+        ]
+        self.assertEqual(split_nodes_link([node]), expected)
+
+    def test_text_to_textnodes(self):
+        text = "This *is* some **markdown** text. It supports `code`, ![images](assets/image.jpg), and [hyperlinks](https://google.fun)"
+        expected = [
+            TextNode("This ", "text"),
+            TextNode("is", "italic"),
+            TextNode(" some ", "text"),
+            TextNode("markdown", "bold"),
+            TextNode(" text. It supports ", "text"),
+            TextNode("code", "code"),
+            TextNode(", ", "text"),
+            TextNode("images", "image", "assets/image.jpg"),
+            TextNode(", and ", "text"),
+            TextNode("hyperlinks", "link", "https://google.fun"),
+        ]
+        self.assertEqual(text_to_textnodes(text), expected)
+
+    def test_markdown_to_blocks(self):
+        input = """
+        This is some very nice markdown text.
+        This is part of the same paragraph.
+
+        This is not.
+        """
+        expected = [
+            "This is some very nice markdown text. This is part of the same paragraph.",
+            "This is not."
+        ]
+        self.assertEqual(markdown_to_blocks(input), expected)
+
 
 if __name__ == "__main__":
     unittest.main()
